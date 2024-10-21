a72618dcd7523043a6e5e65721486a693056175d
s13tc2 <kmk9957@outlook.com>
2024-10-21 14:14:39 -0400

    update k8s tfvars

--- src/terraform/k8s/aws-secrets-manager.tf
+++ src/terraform/k8s/aws-secrets-manager.tf
@@ -1,25 +1,24 @@
 resource "helm_release" "csi_secrets_store" {
-
   name       = "csi-secrets-store"
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
   chart      = "secrets-store-csi-driver"
   namespace  = "kube-system"
+  version    = "1.3.4"  # Specify the latest stable version
 
   set {
     name  = "syncSecret.enabled"
     value = "true"
   }
-
 }
 
-
 resource "helm_release" "aws_secrets_provider" {
-
   name       = "secrets-provider-aws"
   repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
   chart      = "secrets-store-csi-driver-provider-aws"
   namespace  = "kube-system"
+  version    = "0.3.4"  # Specify the latest stable version
 
+  depends_on = [helm_release.csi_secrets_store]
 }
 
 locals {
@@ -28,7 +27,16 @@ locals {
   }
 }
 
+resource "time_sleep" "wait_for_crds" {
+  depends_on = [
+    helm_release.csi_secrets_store,
+    helm_release.aws_secrets_provider
+  ]
+  create_duration = "30s"
+}
+
 resource "kubernetes_manifest" "secret_provider_class" {
+  depends_on = [time_sleep.wait_for_crds]
 
   manifest = {
     apiVersion = "secrets-store.csi.x-k8s.io/v1"
@@ -42,9 +50,9 @@ resource "kubernetes_manifest" "secret_provider_class" {
       parameters = {
         objects = yamlencode([
           {
-            objectName         = "fleet-portal-dev-connection-string"
-            objectType         = "secretsmanager"
-            objectVersionLabel = "AWSCURRENT"
+            objectName           = "fleet-portal-dev-connection-string"
+            objectType           = "secretsmanager"
+            objectVersionLabel   = "AWSCURRENT"
           }
         ])
       }
@@ -62,5 +70,13 @@ resource "kubernetes_manifest" "secret_provider_class" {
       ]
     }
   }
+}
 
+# Optional: Add a null_resource to verify CRD installation
+resource "null_resource" "verify_crd" {
+  depends_on = [time_sleep.wait_for_crds]
+
+  provisioner "local-exec" {
+    command = "kubectl get crd secretproviderclasses.secrets-store.csi.x-k8s.io"
+  }
 }
