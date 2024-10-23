24f5e69d25cc0f3aed4fcfe3234829b94800e721
s13tc2 <kmk9957@outlook.com>
2024-10-23 15:12:35 -0400

    update k8s-apply.yaml

--- src/terraform/k8s/nginx.tf
+++ src/terraform/k8s/nginx.tf
@@ -6,22 +6,47 @@ resource "helm_release" "ingress" {
   create_namespace = true
   namespace        = "ingress-nginx"
 
+  # Override the fullname to ensure consistent naming
+  set {
+    name  = "fullnameOverride"
+    value = "ingress-nginx-controller"
+  }
+
+  # Set the service type to LoadBalancer
   set {
     name  = "service.type"
     value = "LoadBalancer"
   }
+
+  # Set service annotations for AWS NLB
   set {
     name  = "service.annotations"
     value = "service.beta.kubernetes.io/aws-load-balancer-type: nlb"
   }
+
+  # Enable RBAC creation
   set {
     name  = "rbac.create"
     value = "true"
   }
+
+  # Ensure the controller watches all namespaces
   set {
     name  = "controller.scope.enabled"
     value = "false"
   }
+
+  # Create and specify the ServiceAccount
+  set {
+    name  = "controller.serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name  = "controller.serviceAccount.name"
+    value = "ingress-nginx"
+  }
+
+  # Enable and configure the IngressClass resource
   set {
     name  = "ingressClassResource.enabled"
     value = "true"
@@ -32,7 +57,7 @@ resource "helm_release" "ingress" {
   }
   set {
     name  = "ingressClassResource.controllerValue"
-    value = "nginx.org/ingress-controller"
+    value = "k8s.io/ingress-nginx"  # Updated controller value
   }
   set {
     name  = "ingressClass"
