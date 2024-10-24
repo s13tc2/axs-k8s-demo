8025852ae211f2450feb7332a1f1513f75f7591e
s13tc2 <kmk9957@outlook.com>
2024-10-24 10:26:09 -0400

    update

--- src/terraform/k8s/nginx.tf
+++ src/terraform/k8s/nginx.tf
@@ -6,61 +6,13 @@ resource "helm_release" "ingress" {
   create_namespace = true
   namespace        = "ingress-nginx"
 
-  # Override the fullname to ensure consistent naming
-  set {
-    name  = "fullnameOverride"
-    value = "ingress-nginx-controller"
-  }
-
-  # Set the service type to LoadBalancer
   set {
     name  = "service.type"
     value = "LoadBalancer"
   }
-
-  # Set service annotations for AWS NLB, with additional options
   set {
     name  = "service.annotations"
-    value = "service.beta.kubernetes.io/aws-load-balancer-type: nlb, service.beta.kubernetes.io/aws-load-balancer-healthcheck-interval: '20', service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'"
+    value = "service.beta.kubernetes.io/aws-load-balancer-type: nlb"
   }
 
-  # Enable RBAC creation
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
-
-  # Ensure the controller watches all namespaces
-  set {
-    name  = "controller.scope.enabled"
-    value = "false"
-  }
-
-  # Create and specify the ServiceAccount, using the consistent name
-  set {
-    name  = "controller.serviceAccount.create"
-    value = "true"
-  }
-  set {
-    name  = "controller.serviceAccount.name"
-    value = "ingress-nginx-controller"  # Updated to match other references
-  }
-
-  # Enable and configure the IngressClass resource
-  set {
-    name  = "ingressClassResource.enabled"
-    value = "true"
-  }
-  set {
-    name  = "ingressClassResource.name"
-    value = "nginx"
-  }
-  set {
-    name  = "ingressClassResource.controllerValue"
-    value = "k8s.io/ingress-nginx"
-  }
-  set {
-    name  = "ingressClass"
-    value = "nginx"
-  }
 }
